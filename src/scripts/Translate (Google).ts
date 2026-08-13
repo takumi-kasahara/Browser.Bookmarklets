71eee3ead001d13a7d3738555f9b9e9398087e5d
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-08-13 18:58:29 +0900

    fix: refine Google Translate bookmarklet logic for improved URL handling
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

--- src/scripts/Translate (Google).ts
+++ src/scripts/Translate (Google).ts
@@ -1,20 +1,17 @@
 import { is } from '../modules/WindowExtensions.js';
 
 (() => {
-  if (!/https?:/.test(location.protocol)) return;
-  if (location.hostname === 'translate.google.com') return;
-
   const selectedText = document.getSelection?.()?.toString().trim() ?? '';
   if (selectedText) {
     const tl = /[\p{sc=Hiragana}\p{sc=Katakana}\p{sc=Han}]/u.test(selectedText);
     window.open(`https://translate.google.com/?sl=auto&tl=${tl}&text=${encodeURIComponent(selectedText)}&op=translate`, '_blank', 'noreferrer');
+  }
+  else if (location.hostname === 'translate.google.com')
     return;
-  }
-  if (is('translate.goog')) {
-    const e = document.querySelector('[data-source-url]');
-    if (e instanceof HTMLElement && e.dataset.sourceUrl)
-      window.open(e.dataset.sourceUrl, '_blank', 'noreferrer');
-  }
-  if (!document.querySelector('html[lang|=ja]'))
+  else if (is('translate.goog'))
+    return;
+  else if (!/https?:/.test(location.protocol))
+    window.open(`https://translate.google.com/?sl=auto&tl=ja&op=translate}`, '_blank', 'noreferrer');
+  else if (!document.querySelector('html[lang|=ja]'))
     window.open(`https://translate.google.com/translate?sl=auto&tl=ja&u=${location.href}`, '_blank', 'noreferrer');
 })();
