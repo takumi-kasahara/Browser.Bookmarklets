b836e279e538f2f9e0ea1d1f37bb4c7a9b2e623c
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-09-11 10:00:41 +0900

    feat: enhance URL collection in Collect Link script
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

--- src/scripts/Collect Link.ts
+++ src/scripts/Collect Link.ts
@@ -1,13 +1,32 @@
+import { collectAnchorElements } from '../modules/DocumentExtensions.js';
+import { isNotEmptyString } from '../modules/IdentifierExtensions.js';
 import { copyToClipboard } from '../modules/NavigatorExtensions.js';
 import { extractUrlsFromSchema } from '../modules/SchemaExtensions.js';
 
 (async () => {
   if (!/https?:/.test(location.protocol)) return;
 
-  const urls = Array.from(new Set([...extractUrlsFromSchema(document)])).sort(
-    new Intl.Collator(undefined, { numeric: true }).compare,
-  );
+  const urls = Array.from(
+    new Set([
+      ...extractUrlsFromSchema(document),
+      ...collectAnchorElements(document.body, location.origin, true).map(
+        e => e.href,
+      ),
+      ...collectAnchorElements(document.body, location.origin, false).map(
+        e => e.href,
+      ),
+    ]),
+  )
+    .filter(isNotEmptyString)
+    .filter(href => {
+      try {
+        return /https?:/.test(new URL(href).protocol);
+      }
+      catch {
+        return false;
+      }
+    })
+    .sort(new Intl.Collator(undefined, { numeric: true }).compare);
   if (urls.length > 0)
     await copyToClipboard(`Copy ${urls.length} URL(s):`, urls.join('\n'));
-  else await copyToClipboard(`Copy ${urls.length} URL(s):`, location.href);
 })();
